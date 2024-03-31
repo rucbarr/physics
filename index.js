99299d17c6d088a7303bfe390ab0881c1dd352a6
rucbarr <rucbar1998@gmail.com>
2024-03-30 23:08:32 -0500

    Added Ball class, Changed canvas constants to height & weight, Added random & randomRGB helper functions, animated multiple balls simultaneously, created loop/animate function.

--- index.js
+++ index.js
@@ -1,68 +1,92 @@
 const canvas = document.getElementById("gameArea");
 const ctx = canvas.getContext("2d");
 
+// CANVAS CONSTANTS
+const width = (canvas.width);
+const height = (canvas.height);
+
 //PHYSICS CONSTANTS
 const GRAV = 4;
 const BALL_ELAST = .5;
 
-
-
-let r = 15; // radius
-let vx = 20; // X Velocity
-let vy = 20; // Y Velocity
-let x = 50; // X Coordinate
-let y = r; // Y Coordinate
-let coords = [x, y];
-
-let points = [];
-
-function drawGame() {
-    requestAnimationFrame(drawGame);
-    clearScreen();
-
-    //print Circle
-    ctx.fillStyle="green";
-    ctx.beginPath();
-    ctx.arc(coords[0], coords[1], r, 0, 2 * Math.PI);
-    ctx.fill();
-    
-    coords[0] = coords[0] + vx;
-    coords[1] = coords[1] + vy;
-
-    //top&bottom bounding + elasticity
-    if (coords[1] > canvas.height - r){
-        vy = -vy + BALL_ELAST;
-        coords[1] = canvas.height - r;
-    }
-    if(coords[1] < r){
-        vy = -vy - BALL_ELAST;
-        coords[1] = r;
-    }
-
-    //left&right bounding
-    if (coords[0] > canvas.width - r){
-        vx = -vx + BALL_ELAST * 2;
-        coords[0] = canvas.width - r;
-    }
-    if (coords[0] < r){
-        vx = -vx - BALL_ELAST * 2;
-        coords[0] = r;
-    }
-
-    //physics
-    vy = vy + GRAV;
-    if (vx == 0 && vy == 0){
-        stop();
-    }
-
+// random() Helper Function
+// INPUT: min(Minumum random number), max(Maximum random number)
+// OUTPUT: Number between min and max (inclusive)
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function clearScreen() {
+// randomRGB() Helper Function
+// INPUT: N/A
+// OUTPUT: rgb value formatted in http rgb format eg. "rgb(#, #, #)"
+function randomRGB() {
+    return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+}
+class Ball {
+    constructor(x, y, vx, vy, color, r) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.color = color;
+        this.r = r;
+    }
+    update(){
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
 
+        //top&bottom bounding + elasticity
+        if (this.y > height - this.r){
+            this.vy = -this.vy + BALL_ELAST;
+            this.y = height - this.r;
+        }
+        if(this.y < this.r){
+            this.vy = -this.vy - BALL_ELAST;
+            this.y = this.r;
+        }
+
+        //left&right boundingclearScreen();
+        if (this.x > width - this.r){
+            this.vx = -this.vx + BALL_ELAST * 2;
+            this.x = width - this.r;
+        }
+        if (this.x < this.r){
+            this.vx = -this.vx - BALL_ELAST * 2;
+            this.x = this.r;
+        }
+
+        //physics
+        this.vy = this.vy + GRAV;
+    }
+    draw(){
+        ctx.fillStyle=this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+// loop() function
+// INPUT: N/A
+// OUTPUT: N/A
+// Description: loop is used to create the canvas background and to recursively request animation frames. This animates the canvas.
+function loop() {
     ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
+    ctx.fillRect(0, 0, width, height);
 
+    for (const ball of balls){
+        ball.draw();
+        ball.update();
+    }
+    
+    requestAnimationFrame(loop);
 }
 
-drawGame();
+// MAIN FUNCTION
+const balls = [];
+while (balls.length < 100){
+    const ball = new Ball(random(0, width), random(0, height), random(0, 50), random(0, 50), randomRGB(), random(1, 50));
+    balls.push(ball);
+}
+
+loop();
